6ba3371f915f9251c30fbacd7b5e7614870707ab
dagmoller <dagmoller@c7ec44d5-d439-44c5-8c9f-9b209527308a>
2010-03-02 13:02:10 +0000

    Creating branch for tests with twisted

---
+++ branches/twisted/index.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+* Copyright (c) 2008, Diego Aguirre
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* 
+*     * Redistributions of source code must retain the above copyright notice, 
+*       this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, 
+*       this list of conditions and the following disclaimer in the documentation 
+*       and/or other materials provided with the distribution.
+*     * Neither the name of the DagMoller nor the names of its contributors
+*       may be used to endorse or promote products derived from this software 
+*       without specific prior written permission.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require_once 'lib/include.php';
+
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: -1");
+
+session_start();
+$login    = getValor('login', 'session');
+$username = getValor('username', 'session');
+$error = "";
+setValor('started', time());
+setValor('Actions', array());
+$sessionId = session_id();
+session_write_close();
+
+$json     = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+$template = new TemplatePower('template/index.html');
+
+if (!$login)
+{
+	$sock = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	if ($sock === false)
+		$error = "Could not create socket!<br>" . socket_strerror(socket_last_error());
+	else 
+	{
+		$conn = @socket_connect($sock, HOSTNAME, HOSTPORT);
+		if ($conn === false)
+		{
+			$error  = "Could not connect to " . HOSTNAME . ":" . HOSTPORT . " (" . socket_strerror(socket_last_error()) . ").<br>";
+			$error .= "Make sure monast.py is running so the panel can connect to its port properly.";
+		}
+	}
+	
+	if (!$error)
+	{
+		$buffer = "";
+		socket_write($sock, "SESSION: $sessionId");
+		while ($message = socket_read($sock, 1024 * 16)) 
+		{
+			$buffer .= $message;
+			
+			if ($buffer == "NEW SESSION" || $buffer == "OK")
+			{
+				$login = true;
+				session_start();
+				setValor('login', true);
+				session_write_close();
+				socket_write($sock, "BYE");
+			}
+			
+			if ($buffer == "ERROR: Authentication Required")
+			{
+				session_start();
+				setValor('login', false);
+				setValor('username', '');
+				session_write_close();
+				socket_write($sock, "BYE");
+			}
+		}
+		socket_close($sock);
+	}
+}
+
+if ($error)
+{
+	$template->prepare();
+	$template->newBlock('error');
+	$template->assign('errorMessage', $error);
+}
+else
+{
+	if (!$login)
+	{
+		$template->prepare();
+		$template->newBlock('login');
+	}
+	else 
+	{
+		$template->assignInclude('main', 'monast.php');
+		$template->prepare();
+		
+		if ($username)
+			$template->newBlock('buttonLogout');
+	}
+}
+
+$template->printToScreen();
+
+?>
---
+++ branches/twisted/install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+## default paths
+MONASTPATH=/opt/monast
+HTMLPATH=/var/www/html/monast
+BINPATH=/usr/bin
+CONFIGPATH=/etc
+INITD=/etc/init.d
+
+if [ "$(whoami)" != "root" ]; then
+	echo -e "\nYou must be root to run this instalation script...\n"
+	exit 1
+fi
+
+echo
+echo -n "MonAst base path [${MONASTPATH}]: "
+read tmp
+if [ "${tmp}" != "" ]; then
+	MONASTPATH=$tmp
+fi
+
+echo -n "MonAst HTML path [${HTMLPATH}]: "
+read tmp
+if [ "${tmp}" != "" ]; then
+	HTMLPATH=$tmp
+fi
+
+inst=1
+echo 
+if [ -d $MONASTPATH ]; then
+	echo -n "MonAst already instaled on this system. Overrite? [y/n]: "
+	read tmp
+	if [ "${tmp}" != "y" ]; then
+		inst=0
+	fi
+fi
+if [ $inst -eq 1 ]; then
+	mkdir -p $MONASTPATH
+	cp -rf pymon/. $MONASTPATH/
+	echo "MonAst instaled at ${MONASTPATH}"
+	
+	mkdir -p $HTMLPATH
+	cp -rf *.php css image template lib js $HTMLPATH/
+	echo "HTML files instaled at ${HTMLPATH}"
+
+	if [ ! -L $BINPATH/monast ]; then
+		ln -s $MONASTPATH/monast.py $BINPATH/monast
+		echo "Symbolic link to monast.py created at ${BINPATH}/monast"
+	fi
+	
+	if [ ! -f /etc/monast.conf ]; then
+		cp pymon/monast.conf.sample /etc/monast.conf
+		echo "Sample monast.conf created at ${CONFIGPATH}/monast.conf"
+	fi
+
+	if [ -f /etc/slackware-version ]; then
+		cp contrib/slackware/rc.monast /etc/rc.d/rc.monast
+		echo "Instaling rc.d scripts"
+	fi
+
+	if [ -f /etc/redhat-release ]; then
+		cp contrib/init.d/rc.redhat.monast $INITD/monast
+		echo "Instaling init.d scripts"
+	fi
+
+	if [ -f /etc/debian-release ]; then
+		cp contrib/init.d/rc.debian.monast $INITD/monast
+		echo "Instaling init.d scripts"
+	fi
+
+	if [ -f /etc/SuSE-release ]; then
+		cp contrib/init.d/rc.suse.monast $INITD/monast
+		echo "Instaling init.d scripts"
+	fi
+fi
+
+echo
+
