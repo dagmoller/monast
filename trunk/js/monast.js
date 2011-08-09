12834b2d6a1c8561be85c9aed225d601ff46f343
dagmoller <dagmoller@c7ec44d5-d439-44c5-8c9f-9b209527308a>
2011-08-09 12:35:12 +0000

    calculate extra information on queues

--- trunk/js/monast.js
+++ trunk/js/monast.js
@@ -1109,8 +1109,10 @@ var Monast = {
 	queues: new Hash(),
 	processQueue: function (q)
 	{
-		q.id          = md5("queue-" + q.queue);
-		q.contextmenu = null; // FAKE
+		q.id                = md5("queue-" + q.queue);
+		q.contextmenu       = null; // FAKE
+		q.total_calls       = q.completed + q.abandoned;
+		q.abandoned_percent = parseInt((q.abandoned / q.total_calls) * 100);
 		
 		if (Object.isUndefined(this.queues.get(q.id))) // Queue does not exists
 		{
